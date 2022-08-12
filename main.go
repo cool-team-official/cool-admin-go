6d48204b9b6413597c015bebd131faed1ace0922
LiDong <cnlidong@live.cn>
2022-08-12 11:25:30 +0800

    引入demo测试

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "cool-admin-go-simple/internal/packed"
+	_ "cool-admin-go-simple/modules"
 
 	"github.com/gogf/gf/v2/os/gctx"
 
---
+++ modules/modules.go
@@ -0,0 +1,5 @@
+package modules
+
+import (
+	_ "github.com/cool-team-official/cool-admin-go/modules/demo" // 引入模块 demo
+)
