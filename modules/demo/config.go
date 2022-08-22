86de49b592eb7a77f9648fec4c447a9e631aaa45
LiDong <cnlidong@live.cn>
2022-08-22 15:25:37 +0800

    更新代码片断,增加示例模块结构

---
+++ modules/demo/config.go
@@ -0,0 +1,8 @@
+package demo
+
+import (
+	_ "cool-admin-go-simple/modules/demo/controller"
+	_ "cool-admin-go-simple/modules/demo/middleware"
+	_ "cool-admin-go-simple/modules/demo/model"
+	_ "cool-admin-go-simple/modules/demo/service"
+)
