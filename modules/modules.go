7b7dbada236d04aa3f81e18531df7156a698c30b
LiDong <cnlidong@live.cn>
2022-08-23 21:43:52 +0800

    add space modules

--- modules/modules.go
+++ modules/modules.go
@@ -3,4 +3,5 @@ package modules
 import (
 	_ "github.com/cool-team-official/cool-admin-go/modules/base"
 	_ "github.com/cool-team-official/cool-admin-go/modules/dict"
+	_ "github.com/cool-team-official/cool-admin-go/modules/space"
 )
