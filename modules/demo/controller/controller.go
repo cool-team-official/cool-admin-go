07db34863c8fc38fffc1c22c401dfd047069864e
LiDong <cnlidong@live.cn>
2022-08-23 19:02:59 +0800

    增加控制器目录下细分类引入,更新service代码片断

--- modules/demo/controller/controller.go
+++ modules/demo/controller/controller.go
@@ -1,5 +1,7 @@
 package controller
 
 import (
+	_ "cool-admin-go-simple/modules/demo/controller/admin"
+	_ "cool-admin-go-simple/modules/demo/controller/app"
 	_ "cool-admin-go-simple/modules/demo/service"
 )
