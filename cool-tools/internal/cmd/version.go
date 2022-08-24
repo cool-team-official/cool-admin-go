2a63ae0329af639b0fc08f65164205c32b2e6988
LiDong <cnlidong@live.cn>
2022-08-24 10:11:52 +0800

    调整开启godoc

--- cool-tools/internal/cmd/version.go
+++ cool-tools/internal/cmd/version.go
@@ -32,7 +32,7 @@ var (
 
 			var binVersion string
 			if len(data) == 0 || data["version"] == nil {
-				binVersion = "v0.0.6-dev"
+				binVersion = "v0.0.8-dev"
 			} else {
 				binVersion = data["version"].(string)
 			}
