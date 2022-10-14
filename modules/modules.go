5c46cc5ce322f7000b2e0c84abae862aa0f0a0d8
LiDong <cnlidong@live.cn>
2022-10-14 21:05:47 +0800

    更新模板

--- modules/modules.go
+++ modules/modules.go
@@ -4,4 +4,5 @@ import (
 	_ "github.com/cool-team-official/cool-admin-go/modules/base"
 	_ "github.com/cool-team-official/cool-admin-go/modules/dict"
 	_ "github.com/cool-team-official/cool-admin-go/modules/space"
+	_ "github.com/cool-team-official/cool-admin-go/modules/task"
 )
