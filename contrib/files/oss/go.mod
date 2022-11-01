449f50db4255d1c2d1c6064cca0d5bf04bcf4392
LiDong <cnlidong@live.cn>
2022-11-01 22:43:42 +0800

    oss minio 分享为单独组件

---
+++ contrib/files/oss/go.mod
@@ -0,0 +1,7 @@
+module github.com/cool-team-official/cool-admin-go/contrib/files/oss
+
+go 1.18
+
+require github.com/aliyun/aliyun-oss-go-sdk v2.2.5+incompatible
+
+require golang.org/x/time v0.1.0 // indirect
--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	_ "github.com/cool-team-official/cool-admin-go/contrib/files/local"
 
 	// Minio，按需启用
-	//_ "github.com/cool-team-official/cool-admin-go/contrib/files/minio"
+	_ "github.com/cool-team-official/cool-admin-go/contrib/files/minio"
 
 	// 阿里云OSS，按需启用
-	//_ "github.com/cool-team-official/cool-admin-go/contrib/files/oss"
+	_ "github.com/cool-team-official/cool-admin-go/contrib/files/oss"
 
 	_ "github.com/cool-team-official/cool-admin-go/contrib/drivers/sqlite"
 
