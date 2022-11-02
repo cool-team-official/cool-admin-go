126c488af204e85db3bb7be1448f810a06989b87
LiDong <cnlidong@live.cn>
2022-11-02 10:04:32 +0800

    各模块独立维护依赖包信息

--- gotidy.sh
+++ gotidy.sh
@@ -11,15 +11,14 @@ find $DIR -name "go.mod" | while read line; do
     # 获取go.mod文件所在目录
     MOD_DIR=$(dirname $line)
     echo "MOD_DIR: $MOD_DIR/go.mod"
-    # cd $MOD_DIR
-    # rm go.sum
-    # go get -u
-    # go mod tidy
+    cd $MOD_DIR
+    go get -u
+    go mod tidy
 done
 
-go get -u
-go mod tidy
+# go get -u
+# go mod tidy
 
-cd cool-tools
-go get -u
-go mod tidy
+# cd cool-tools
+# go get -u
+# go mod tidy
