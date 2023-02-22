98f469660a8d17c655caebb43efdebef0e4a8055
Li Dong <cnlidong@live.cn>
2023-02-22 16:31:23 +0800

    1.0.17
    - list page 接口增加 ModifyAfter 钩子,用于对数据进行修改
    - 更新 gf 至 v2.3.2

--- cool-tools/internal/cmd/version.go
+++ cool-tools/internal/cmd/version.go
@@ -27,7 +27,7 @@ var (
 		Brief: "查看版本信息",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			info := gbuild.Info()
-			binVersion := "v1.0.16"
+			binVersion := "v1.0.17"
 
 			// 生成sVersion结构体
 			res := sVersion{
